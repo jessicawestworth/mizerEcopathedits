e4bcd46c30fcf90a2c52d58c84753d8bc36a3523
Jessica Westworth <jwestworth04@gmail.com>
2025-12-05 11:16:37 +0000

    in species params scientific name is held as Scientific_name, not SciName, but will depend based on how the data frame for species parameters is initially set up.

--- R/ageDensity.R
+++ R/ageDensity.R
@@ -266,7 +266,7 @@ simulate_sample_from_model <- function(P_model_K_given_l, survey_obs) {
 #' # Using package data would typically look like:
 #' # df <- preprocess_length_at_age(params, species = "Cod", age_at_length = your_df)
 preprocess_length_at_age <- function(params, species, age_at_length) {
-    sci_name <- species_params(params)[species, "SciName"]
+    sci_name <- species_params(params)[species, "Scientific_name"]
     survey_dates <- c(0.125, 0.375, 0.625, 0.875)
     age_at_length |>
         filter(.data$Scientific_name == sci_name) |>
